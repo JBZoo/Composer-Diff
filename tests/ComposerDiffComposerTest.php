aac90b79565f7a8e736f4f7aa06a86c1f690cc45
Denis Smetannikov <smet.denis@gmail.com>
2020-07-15 02:10:05 +0300

    Fixes versions and tests

---
+++ tests/ComposerDiffComposerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * JBZoo Toolbox - Composer-Diff
+ *
+ * This file is part of the JBZoo Toolbox project.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package    Composer-Diff
+ * @license    MIT
+ * @copyright  Copyright (C) JBZoo.com, All rights reserved.
+ * @link       https://github.com/JBZoo/Composer-Diff
+ */
+
+namespace JBZoo\PHPUnit;
+
+/**
+ * Class ComposerDiffComposerTest
+ *
+ * @package JBZoo\PHPUnit
+ */
+class ComposerDiffComposerTest extends AbstractComposerTest
+{
+
+}
